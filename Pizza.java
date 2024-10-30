711e8ae9703e67e2e83b599164e02d8b5b962a5c
NikoZK <nikodem09@live.dk>
2024-10-30 10:02:21 +0100

    Niko 30-10-24 Pizza hyggerettelser

--- Pizza.java
+++ Pizza.java
@@ -2,13 +2,11 @@ public class Pizza {
     private int pris;
     private String navn;
     private String nummer;
-    private int count; // Ny variabel til at tælle bestillinger
 
     public Pizza(String nummer, String navn, int pris) {
         this.nummer = nummer;
         this.pris = pris;
         this.navn = navn;
-        this.count = 0; // Initialiser count til 0
     }
 
     public int getPris() {
@@ -21,14 +19,6 @@ public class Pizza {
 
     public String getNummer() {
         return nummer;
-    }
-
-    public int getCount() {
-        return count; // Hent antallet af bestillinger
-    }
-
-    public void incrementCount() {
-        this.count++; // Øg antallet med 1
     }
 
     public void setNummer(String nummer) {
@@ -45,6 +35,6 @@ public class Pizza {
 
 
     public String toString() {
-        return " " + nummer + ". " + navn + " " + pris + "kr "; // Tilføjet count til toString
+        return " " + nummer + ". " + navn + " " + pris + "kr "; //toString så vi printer noget læsbart
     }
 }
